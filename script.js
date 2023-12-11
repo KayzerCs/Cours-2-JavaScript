d8840e18471ba699ed9f17dec082f13877a42ed8
Kayzer <max.mlv2210@gmail.com>
2023-12-11 18:41:59 +0100

    Sélécteurs

---
+++ script.js
@@ -0,0 +1,9 @@
+// SELECTEURS
+
+// document.querySelector("h4").style.background = "yellow";
+
+// Mettre l'élement dans une variable
+// const baliseHTML = document.querySelector("h4");
+// console.log(baliseHTML);
+// baliseHTML.style.background = "yellow";
+
