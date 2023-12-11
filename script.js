914f81cb0fec3e606c2d59109ba56fc43e4691c6
Kayzer <max.mlv2210@gmail.com>
2023-12-11 20:08:41 +0100

    Fin de journée

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 // SELECTEURS
 
-// document.querySelector("h4").style.background = "yellow";
+//Pour selectionné une "balise" avec JS -> document.querySelector("h4");
+//Pour selectionné une "balise" en CSS il faut -> Ex: ("h4");
+//Pour selectionné une "classe" en HTML il faut mettre un (.) en plus -> Ex: (".myTilte");
+//Pour selectionné un "id" en HTML il faut mettre un (#) en plus -> Ex: ("#myTilte");
 
-// Mettre l'élement dans une variable
+// On peut faire du "CSS" avec JS -> document.querySelector("h4").style.background = "yellow";
+
+// Mettre le Selecteur dans une variable est une méthode plus moderne.
 // const baliseHTML = document.querySelector("h4");
 // console.log(baliseHTML);
 // baliseHTML.style.background = "yellow";
 
+// Click event
+const questionContainer = document.querySelector(".click-event");
+
+// questionContainer.addEventListener("événement", () function)
+questionContainer.addEventListener("click", () => {
+  questionContainer.style.background = "red";
+  questionContainer.style.borderRadius = "250px";
+  questionContainer.style.color = "yellow";
+});
